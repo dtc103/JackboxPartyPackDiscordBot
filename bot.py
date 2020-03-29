bcc0c17eb1cb5fb3af7c8056349545c9b8fc55f9
dtc103 <jan-kerner@web.de>
2020-03-30 01:58:57 +0200

    extended queue

--- bot.py
+++ bot.py
@@ -2,9 +2,12 @@
 import os
 import discord
 import random
+import asyncio
+import typing
+import re
 
 from dotenv import load_dotenv
-from discord.ext import commands
+from discord.ext import commands, tasks
 
 load_dotenv()
 
@@ -12,39 +15,82 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 GUILD = os.getenv("DISCORD_GUILD")
 WORKING_CHANNEL = os.getenv("WORKING_CHANNEL_ID")
 JACKBOX_VOICECHANNEL_CATEGORY = os.getenv("JACKBOX_VOICECHANNEL_CATEGORY")
+DEFAULT_QUEUE = os.getenv("DEFAULT_QUEUE")
+DEFAULT_QUEUE_MINIMUM_REQ = 4
 
 # client = discord.Client()
 bot = commands.Bot(command_prefix="!")
 
+# because jackbox 1 and jackbox 2 are fixed channels
+smallest_poss_number_of_channel = 3
+# if unnamed queue, just add a number
+current_smallest_number = 3
+# maximum amount of jackbox voicechannels
+maximum_channel_number = 20
 
-# @bot.event
-# async def on_message(message):
-# if message.author == bot.user:
-#     return
+wtp_queues = []
 
-# channel_id = discord.utils.find(
-#     lambda channel: channel.name == message.channel.name, message.guild.channels)
 
-# await message.channel.send(f"The message {message.content} was sent in {channel_id.mention}")
+################# CLASSES #####################
+
+class QueueUser:
+    def __init__(self, member, queue):
+        self.member = member
+        self.queue = queue
+        self.livetime = 0
+
+    def __eq__(self, other):
+        return self.member == other.member
+
+
+class IndividQueue:
+    def __init__(self, originator: QueueUser, queue_name: str, min_req: int):
+        self.userlist = []
+        if originator is not None:
+            self.userlist.append(originator)
+        self.originator = originator
+        self.name = queue_name
+        self.min_req = min_req
+
+    def append(self, queue_user: QueueUser):
+        self.userlist.append(queue_user)
+
+    def remove(self, queue_user: QueueUser):
+        self.userlist.remove(queue_user)
+
+    def __len__(self):
+        return len(self.userlist)
+
+    def __eq__(self, other):
+        return self.name == other.name
+
 
 ##################### HELPER FUNCTIONS ###########################
+
+
 def get_current_guild():
     return discord.utils.find(lambda g: GUILD == g.name, bot.guilds)
 
 
-def get_jackbox_voicechannels():
-    return discord.utils.find(lambda c: JACKBOX_VOICECHANNEL_CATEGORY == c.name.lower(), get_current_guild().categories).channels
+def get_current_jackbox_vcs():
+    return discord.utils.find(lambda c: JACKBOX_VOICECHANNEL_CATEGORY == c.name, get_current_guild().categories).channels
 
 
-def manage_channels():
-    pass
+async def manage_channels():
+    jack_calls = get_current_jackbox_vcs()
+    for vc in jack_calls:
+        if vc.name == "jackbox 1" or vc.name == "jackbox 2":
+            continue
+        else:
+            if len(vc.members) == 0:
+                await vc.delete(reason="Empty")
 
 
 def bot_command_channel(channel="searching-for-players"):
-    async def wrapper_for_check(ctx):
+    async def wrapper_for_check(ctx, *args):
         if ctx.message.channel.name != channel:
-            await ctx.send(f"{ctx.message.author.mention}, this botcommand belongs into {discord.utils.find(lambda c: channel == c.name, ctx.message.guild.channels).mention}", delete_after=10)
-            await ctx.message.delete(delay=10)
+            await ctx.send(f"{ctx.message.author.mention}, this botcommand belongs into {discord.utils.find(lambda c: channel == c.name, ctx.message.guild.channels).mention}", delete_after=20)
+            await ctx.message.delete(delay=20)
 
             return False
         else:
@@ -52,15 +98,94 @@ def bot_command_channel(channel="searching-for-players"):
 
     return commands.check(wrapper_for_check)
 
+
+def queue_name_is_valid(channelname: str):
+    if re.match("jackbox \d+", channelname.lower()) or channelname.lower() == "default" or channelname == DEFAULT_QUEUE:
+        return False
+    else:
+        return True
+
+
+def user_already_in_any_queue(queue_user_to_check: discord.Member):
+    for queue in wtp_queues:
+        for queue_user in queue.userlist:
+            if queue_user_to_check == queue_user.member:
+                return queue_user
+
+    return None
+
+
+def get_queue(queue_name: str):
+    if queue_name == DEFAULT_QUEUE:
+        return wtp_queues[0]
+
+    for queue in wtp_queues:
+        if queue_name == queue.name:
+            return queue
+    return None
+
+# adds a user to a exisitng queue
+
+
+def add_user_to_queue(member: discord.Member, queuename: str):
+    queue = get_queue(queuename)
+    new_user = QueueUser(member, queue)
+    queue.append(new_user)
+    return queue
+
+# creates a new queue and adds the originator to it
+
+
+def add_queue(originator: discord.Member, queuename: str, min_req: int):
+    new_originator = QueueUser(originator, None)
+    queue = IndividQueue(new_originator, queuename, min_req)
+    new_originator.queue = queue
+    wtp_queues.append(queue)
+    return queue
+
+# checks if a queue has enough member to create a party
+
+
+def queue_is_full(queuename):
+    if isinstance(queuename, IndividQueue):
+        return len(queuename) >= queuename.min_req
+    elif isinstance(queuename, str):
+        queue = get_queue(queuename)
+        if len(queue) >= queue.min_req:
+            return True
+        else:
+            return False
+
+# create the response message, if the queue is full
+
+
+def queue_full_response(queue: IndividQueue):
+    response = "Hey "
+    for index, queue_user in enumerate(queue.userlist):
+        response += f"{queue_user.member.mention}, "
+
+    response += "finally you are enough players for a new party. Have fun!!!"
+
+    return response
+
+
+def delete_queue():
+    pass
+
 ##################### EVENTS AND COMMANDS #######################################
+
+# async def check_queue():
+#     while not bot.is_closed():
+#         print("henlo")
+#         await asyncio.sleep(5)
 
 
 @bot.event
 async def on_ready():
-    guild = get_current_guild()
-
-    members = guild.members
-    roles = guild.roles
+    wtp_queues.append(IndividQueue(
+        None, DEFAULT_QUEUE, DEFAULT_QUEUE_MINIMUM_REQ))
+    await manage_channels()
+    print("bot started")
 
 
 @bot.event
@@ -79,11 +204,6 @@ async def on_reaction_add(reaction, user):
     print(f"added {reaction.message} reaction, by {user.name}")
     await reaction.message.channel.send(f"added {reaction.emoji} reaction, by {user.name}")
 
-wtp_player_list = []
-smallest_possible_number_of_channel = 3
-current_smallest_number = 3
-maximum_channel_number = 20
-
 
 @bot.event
 async def on_voice_state_update(member, before, after):
@@ -101,40 +221,58 @@ async def on_voice_state_update(member, before, after):
 
 @bot.event
 async def on_command_error(ctx, error):
-    print("ERROR OCCURED")
+    print("Seems like the command was wrong")
 
 
-# wenn nach x minuten kein neuer user mehr dazu kommt, muss die liste gelehrt werden
-@bot.command(name="wtp", help="Searching for some companions to play with")
+@bot.command(name="wtp", help="Searching for some companions to play with.\n Usage... ")
 @bot_command_channel("searching-for-players")
-async def want_to_play(ctx):
-    minimum_amount_of_players = 4
+async def want_to_play(ctx, channelname: str = None, channelsize=10):
+    user = user_already_in_any_queue(ctx.author)
 
-    if ctx.message.author in wtp_player_list:
-        await ctx.send(f"Sorry {ctx.message.author.mention}, but you are already in the queue. Wait for other players to show up.")
+    # check, if user is already in a queue
+    if user is not None:
+        await ctx.send(f"{ctx.author.mention} you are already in the **{user.queue.name}** queue")
+        return
+
+    # check if the queue already exists
+    if channelname is None:
+        curr_queue = add_user_to_queue(ctx.author, DEFAULT_QUEUE)
+        if queue_is_full(curr_queue):
+            await ctx.send(queue_full_response(curr_queue))
+        else:
+            await ctx.send(f"There are currently **{len(get_queue(DEFAULT_QUEUE))}** people waiting in **{get_queue(DEFAULT_QUEUE).name}** queue")
     else:
-        wtp_player_list.append(ctx.message.author)
-        await ctx.send(f"{len(wtp_player_list)} players wants to play currently")
-
-    if(len(wtp_player_list) >= minimum_amount_of_players):
-        reply_message = "Hey "
-        for index, player in enumerate(wtp_player_list):
-            if index == len(wtp_player_list) - 1:
-                reply_message += f" and {player.mention}"
+        opt_queue = get_queue(channelname)
+        if opt_queue is None:
+            add_queue(ctx.author, channelname, channelsize)
+            await ctx.send(f"{ctx.author.mention} created the queue: **{channelname}**. Feel free to join!")
+        else:
+            curr_queue = add_user_to_queue(ctx.author, channelname)
+            if queue_is_full(curr_queue):
+                await ctx.send(queue_full_response(curr_queue))
+                delete_queue(curr_queue)
             else:
-                reply_message += f", {player.mention}"
-        reply_message += ", finally you are enough players for a new Jackbox session. Have fun!!!"
+                await ctx.send(f"There are currently **{len(get_queue(channelname))}** people waiting in the **{get_queue(channelname).name}** queue")
 
-        manage_channels(wtp_player_list)
+        # if(len(wtp_player_list) >= minimum_amount_of_players):
+        #     reply_message = "Hey "
+        #     for index, player in enumerate(wtp_player_list):
+        #         if index == len(wtp_player_list) - 1:
+        #             reply_message += f" and {player.mention}"
+        #         else:
+        #             reply_message += f", {player.mention}"
+        #     reply_message += ", finally you are enough players for a new Jackbox session. Have fun!!!"
 
-        await ctx.send(reply_message)
+        #     # TODO add new channel
 
-        wtp_player_list.clear()
+        #     await ctx.send(reply_message)
+
+        #     wtp_player_list.clear()
 
 
 @bot.command(name="wtp-info", help="show the number of people, currently searching for players")
 @bot_command_channel("searching-for-players")
-async def want_to_play_info(ctx):
+async def want_to_play_info(ctx, queue_name: str = None):
     reply_message = ""
     if(len(wtp_player_list) == 1):
         reply_message += f"```This {len(wtp_player_list)} player is currently waiting for more people:\n"
@@ -144,6 +282,19 @@ async def want_to_play_info(ctx):
         reply_message += player.name + "\n"
     reply_message += "```"
     await ctx.send(reply_message)
+
+
+@bot.command(name="wtp-switch", help="switch to another queue")
+@bot_command_channel("searching-for-players")
+async def switch_queue(ctx, queue_name: str):
+    pass
+
+
+@bot.command(name="wtp-leave", help="leave the current queue")
+@bot_command_channel("searching-for-players")
+async def leave_queue(ctx):
+    if ctx.author in wtp_player_list:
+        wtp_all_queue_members.remove(ctx.author)
 
 
 bot.run(TOKEN)
